0966d83ce964df383058ac1ea564c15ec519e9d4
osamuel-cardoso <108777753+osamuel-cardoso@users.noreply.github.com>
2023-01-11 04:52:20 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,11 @@
 const swiperProcess = new Swiper(".swiper.process", {
   slidesPerView: "auto",
   spaceBetween: 20,
+  
+  navigation: {
+    nextEl: "control_left-arrow",
+    prevEl: "control_right-arrow"
+  },
 
   breakpoints: {
     480: {
